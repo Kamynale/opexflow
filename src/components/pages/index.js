90062811b3bd5520a19979a5e64626d3a87e8f80
Kamynale <alexandrkamynin@gmail.com>
2020-11-19 12:40:00 +0300

    exchange

---
+++ src/components/pages/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render, hydrate } from 'react-dom';
+import { Provider } from 'react-redux';
+import Loadable from 'react-loadable';
+import { Frontload } from 'react-frontload';
+import { ConnectedRouter } from 'connected-react-router';
+import createStore from './store';
+
+import ExchangeRate from './ExchangeRate
+
+// Create a store and get back itself and its history object
+const { store, history } = createStore();
+
+
+const Application = (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Frontload noServerRender={true}>
+                <ExchangeRate />
+            </Frontload>
+        </ConnectedRouter>
+    </Provider>
+);
+
+const root = document.querySelector('#root');
+
+if (root.hasChildNodes() === true) {
+    Loadable.preloadReady().then(() => {
+        hydrate(Application, root);
+    });
+} else {
+    render(Application, root);
+}
